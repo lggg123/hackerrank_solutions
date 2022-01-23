2cb07bb1c8c951fa99cdcb0de2b956445e956ca9
lggg123 <machinelearner2334@gmail.com>
2022-01-22 20:21:59 -0800

    added more

---
+++ breaking_records/solution.rb
@@ -0,0 +1,4 @@
+# Maria tabulates the number of times she breaks her season record for most points
+# and least points in a game. 
+# Game  Score  Minimum  Maximum  Count(Min) Count(Max)
+#   0    12       12       12        0         0      
